c002310d4b00336926ee588eb33172b34bc52846
yilmazcinar <cinaryilmaz96@gmail.com>
2024-12-17 14:49:59 +0300

    Project files added

---
+++ myFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ben Yılmaz ÇINAR, patika+ programına kendime farklı bir kariyer çizebilmek için katıldım");
+        }
+    }
+}
